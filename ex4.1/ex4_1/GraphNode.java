2189150ef84a11a808ff5b1cd50532337a919dba
lokkochan <chunlok.chan@ucalgary.ca>
2023-03-29 17:29:11 -0600

    ex4.1 kind of done

---
+++ ex4.1/ex4_1/GraphNode.java
@@ -0,0 +1,30 @@
+package ex4_1;
+import java.util.LinkedList;
+
+public class GraphNode{
+    private String value;
+    private LinkedList<edges> neighbors;
+
+    public GraphNode(String value){
+        this.value = value;
+        this.neighbors = new LinkedList<edges>();
+    }
+
+    public void addNeighbor(GraphNode node, int weight){
+        edges neighbor = new edges(node, weight);
+        neighbors.add(neighbor);
+    }
+
+    public void removeNeighbor(GraphNode node){
+        for(edges e : neighbors){
+            if(e.getNode() == node){
+                neighbors.remove(e);
+                break;
+            }
+        }
+    }
+    public String getValue(){
+        return value;
+    }
+
+}
---
+++ ex4.1/ex4_1/edges.java
@@ -0,0 +1,19 @@
+package ex4_1;
+
+public class edges{
+    private GraphNode node;
+    private int weight;
+
+    public edges(GraphNode node, int weight){
+        this.node = node;
+        this.weight = weight;
+    }
+
+    public GraphNode getNode(){
+        return node;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+}
